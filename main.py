6bc3092264835cd9937c6cf66bfb408cb501a74c
khmel <khmelev.vladislav@gmail.com>
2024-05-22 20:25:34 +1000

    Реализовано изменение всех таблиц.

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import recources
 
 from PIL import Image
 from io import BytesIO
-from PyQt6.QtCore import Qt, QDate
+from PyQt6.QtCore import Qt, QDate, QTime
 from PyQt6 import uic, QtWidgets, QtCore
-from PyQt6.QtWidgets import QMainWindow, QDialog, QFileDialog, QLabel
+from PyQt6.QtWidgets import QMainWindow, QDialog, QFileDialog, QLabel, QTableWidgetItem, QTableWidget
 from PyQt6.QtGui import QPixmap, QIcon
 
 Current_login, info = "", ""
@@ -25,6 +25,8 @@ class SignInWindow(QMainWindow):
             self.sign_up_click()
         self.sign_up_label.mousePressEvent = mousePressEvent
         self.sign_in_button.clicked.connect(self.sign_in_click)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
 
     def sign_in_click(self):
         global Current_login, info
@@ -48,6 +50,8 @@ class UserSignUpWindow(QDialog):
         super(UserSignUpWindow, self).__init__()
         uic.loadUi('Ui\\sign_up_window.ui', self)
         self.sign_up_pushButton.clicked.connect(self.data_check)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
 
     def print_error(self,error_type):
         match error_type:
@@ -89,6 +93,79 @@ class UserSignUpWindow(QDialog):
                     self.close()
                     auth_window.show()
 
+
+class BookRentWindow(QDialog):
+    def __init__(self, book_info):
+        super(BookRentWindow, self).__init__()
+        uic.loadUi('Ui\\book_rent_window.ui', self)
+        self.book_pushButton.clicked.connect(self.rent_book)
+        self.current_book = book_info
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
+    def rent_book(self):
+        requests.rent_book(self.current_book, Current_login)
+
+
+class ClickableImageLabel(QLabel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            book_info = requests.search_book(self.objectName())
+            self.book_rent_window = BookRentWindow(book_info)
+            self.book_rent_window.show()
+            self.book_rent_window.book_name_label.setText(book_info[2])
+            self.book_rent_window.author_label.setText(book_info[1])
+            self.book_rent_window.date_label.setText(book_info[3])
+            self.book_rent_window.description_plainTextEdit.setPlainText(book_info[5])
+            self.book_rent_window.quantity_label.setText(str(book_info[6]))
+            self.book_rent_window.library_label.setText(book_info[0])
+
+
+class ChangePasswordWindow(QDialog):
+    def __init__(self):
+        super(ChangePasswordWindow, self).__init__()
+        uic.loadUi('Ui\\change_password_window.ui', self)
+        self.error_label.hide()
+        self.pushButton.clicked.connect(self.password_change)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
+
+    def password_change(self):
+        if requests.auth(Current_login, self.current_pass_lineEdit.text()):
+            requests.update_info('Auth', 'Password', 'Login', Current_login, self.new_pass_lineEdit.text())
+            open_windows = app.topLevelWidgets()
+            for window in open_windows:
+                window.close()
+            auth_window = SignInWindow()
+            auth_window.show()
+
+
+class ChangeLoginWindow(QDialog):
+    def __init__(self):
+        super(ChangeLoginWindow, self).__init__()
+        uic.loadUi('Ui\\change_login_window.ui', self)
+        self.error_label.hide()
+        self.login_label.setText(Current_login)
+        self.pushButton.clicked.connect(self.login_change)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
+    def login_change(self):
+        if requests.is_row_exist('Auth', 'Login', self.login_lineEdit.text()):
+            self.error_label.setText('Этот логин уже занят!')
+        else:
+            if requests.auth(Current_login, self.password_lineEdit.text()):
+                requests.update_info('Auth', 'Login', 'Login', Current_login, self.login_lineEdit.text())
+                open_windows = app.topLevelWidgets()
+                for window in open_windows:
+                    window.close()
+                auth_window = SignInWindow()
+                auth_window.show()
+            else:
+                self.error_label.setText('Неверный пароль!')
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -99,13 +176,25 @@ class MainWindow(QMainWindow):
         self.remove_book_action.triggered.connect(self.remove_book)
         self.library_choose_comboBox.currentIndexChanged.connect(self.update_books)
         self.library_choose_comboBox.addItems(requests.select_libraries())
+        self.edit_library_action.triggered.connect(self.edit_lib)
         self.change_number_action.triggered.connect(self.change_num)
         self.reload_pushButton.clicked.connect(self.update_books)
+        self.change_user_info_action.triggered.connect(self.change_user_info)
+        self.delete_library_action.triggered.connect(self.delete_library)
+        self.change_staff_info_action.triggered.connect(self.change_staff_info)
+        self.change_login_action.triggered.connect(self.change_login)
+        self.change_password_action.triggered.connect(self.change_password)
+        self.add_library_action.triggered.connect(self.add_library)
+        requests.del_expired_books()
         cur_lib = requests.select_libraries()[0]
         self.display_books(cur_lib)
         if info[0] == "Reader":
             self.menu.clear()
             self.menu.setTitle("")
+            self.menu_2.clear()
+            self.menu_2.setTitle("")
+            self.menu_3.clear()
+            self.menu_3.setTitle("")
         icon = QIcon(":/images/images/icon.ico")  # указываем путь к иконке в формате ":/путь/к/ресурсу"
         #reload_ico = QPixmap(":/images/images/reload.ico")
         #self.reload_icon.setPixmap(reload_ico)
@@ -114,12 +203,12 @@ class MainWindow(QMainWindow):
     def update_books(self):
         selected_lib = self.library_choose_comboBox.currentText()
         self.display_books(selected_lib)
-
     def display_books(self, table):
         notes = requests.parse_notes("Books")
         layout = QtWidgets.QGridLayout()
         string = 0
         column = 0
+        #books_dict = {}
         for book in range(requests.notes_count("Books")):
             note = notes[book][6]
             if notes[book][1].strip() != table.strip():
@@ -133,8 +222,9 @@ class MainWindow(QMainWindow):
                 pixmap.loadFromData(image_data)
             else:
                 pixmap = QPixmap(":/images/images/default.png")
+            #books_dict[notes[book][1]] = notes[book][0]
             scaled_pixmap = pixmap.scaled(200, 313)
-            img = QtWidgets.QLabel('label')
+            img = ClickableImageLabel('label')
             stack = QtWidgets.QWidget()
             title = QtWidgets.QLabel(notes[book][2])
             stack_layout = QtWidgets.QVBoxLayout()
@@ -142,6 +232,7 @@ class MainWindow(QMainWindow):
             stack_layout.addWidget(title)
             stack.setLayout(stack_layout)
             layout.addWidget(stack, string, column, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+            img.setObjectName(str(notes[book][0]))
             img.setPixmap(scaled_pixmap)
             column += 1
             if column == 3:
@@ -171,18 +262,192 @@ class MainWindow(QMainWindow):
     def remove_book(self):
         self.book_remove_window = BookRemoveWindow()
         self.book_remove_window.show()
+    def change_user_info(self):
+        self.change_user_info_window = ChangeUserInfoWindow()
+        self.change_user_info_window.show()
+    def change_staff_info(self):
+        self.change_staff_info_window = ChangeStaffInfoWindow()
+        self.change_staff_info_window.show()
+    def rent_book(self, book_info):
+        self.book_rent_window = BookRentWindow()
+        self.book_rent_window.show()
+        self.book_rent_window.book_name_label.setText(book_info[2])
 
+    def change_login(self):
+        self.change_login_window = ChangeLoginWindow()
+        self.change_login_window.show()
+
+    def change_password(self):
+        self.change_password_window = ChangePasswordWindow()
+        self.change_password_window.show()
+
+    def add_library(self):
+        self.add_library_window = AddLibraryWindow()
+        self.add_library_window.show()
+
+    def delete_library(self):
+        self.delete_library_window = DeleteLibraryWindow()
+        self.delete_library_window.show()
+
+    def edit_lib(self):
+        self.edit_library_window = EditLibraryWindow()
+        self.edit_library_window.show()
+
+class ChangeUserInfoWindow(QDialog):
+    def __init__(self):
+        super(ChangeUserInfoWindow, self).__init__()
+        uic.loadUi('Ui\\change_user_info_window.ui', self)
+        self.applyButton.clicked.connect(self.change_data)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
+        readers = requests.parse_notes("Readers")
+        row = 0
+        for user in readers:
+            self.usersTable.insertRow(self.usersTable.rowCount())
+            for column, value in enumerate(user):
+                item = QTableWidgetItem(str(value).rstrip())
+                if column == 0 or column == 1:
+                    item.setFlags(Qt.ItemFlag.ItemIsEditable)
+                self.usersTable.setItem(row, column, item)
+            row +=1
+
+    def change_data(self):
+        all_rows = []
+        num_rows = self.usersTable.rowCount()
+        num_columns = self.usersTable.columnCount()
+        for row in range(num_rows):
+            current_row = []
+            for column in range(num_columns):
+                item = self.usersTable.item(row, column)
+                if item is not None:
+                    current_row.append(item.text())
+                else:
+                    current_row.append("")  # Если ячейка пуста, добавляем пустую строку
+            all_rows.append(current_row)
+        requests.update_users_info(all_rows)
+
+
+class EditLibraryWindow(QDialog):
+    def __init__(self):
+        super(EditLibraryWindow, self).__init__()
+        uic.loadUi('Ui\\changeLibraryInfo.ui', self)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
+        self.library_comboBox.addItems(requests.select_libraries())
+        self.library_comboBox.currentIndexChanged.connect(self.choose_lib)
+        self.pushButton.clicked.connect(self.update_info)
+
+    def choose_lib(self):
+        lib_info = requests.parse_row("Libraries", "Library_Name", self.library_comboBox.currentText())
+        if lib_info != None:
+            self.adress_lineEdit.setText(lib_info[1].rstrip())
+            open_time = QTime.fromString(lib_info[2][0:5], 'hh:mm')
+            self.opening_timeEdit.setTime(open_time)
+            close_time = QTime.fromString(lib_info[3][0:5], 'hh:mm')
+            self.closing_timeEdit.setTime(close_time)
+            self.phone_lineEdit.setText(lib_info[4])
+            self.computer_class_checkBox.setChecked(bool(lib_info[5]))
+            self.reading_room_checkBox.setChecked(bool(lib_info[6]))
+        else:
+            time = QTime.fromString('00:00', 'hh:mm')
+            self.adress_lineEdit.clear()
+            self.opening_timeEdit.setTime(time)
+            self.closing_timeEdit.setTime(time)
+            self.phone_lineEdit.clear()
+            self.computer_class_checkBox.setChecked(False)
+            self.reading_room_checkBox.setChecked(False)
+
+    def update_info(self):
+        requests.update_library_info(self.library_comboBox.currentText(),
+                                     self.adress_lineEdit.text().rstrip(),
+                                     self.opening_timeEdit.time().toString("HH:mm"),
+                                     self.closing_timeEdit.time().toString("HH:mm"),
+                                     self.phone_lineEdit.text().replace('(','').replace('-','').replace(')',''),
+                                     str(self.computer_class_checkBox.isChecked()),
+                                     str(self.reading_room_checkBox.isChecked()))
+
+class DeleteLibraryWindow(QDialog):
+    def __init__(self):
+        super(DeleteLibraryWindow, self).__init__()
+        uic.loadUi('Ui\\deleteLibraryWindow.ui', self)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
+        self.del_pushButton.clicked.connect(self.delete)
+        self.library_comboBox.addItems(requests.select_libraries())
+
+    def delete(self):
+        requests.del_library(self.library_comboBox.currentText())
+        self.close()
+
+
+class AddLibraryWindow(QDialog):
+    def __init__(self):
+        super(AddLibraryWindow, self).__init__()
+        uic.loadUi('Ui\\addLibraryWindow.ui', self)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
+        self.pushButton.clicked.connect(self.add_info)
+        self.error_label.hide()
+    def add_info(self):
+        if (self.library_name_lineEdit.text() != '' and
+            self.adress_lineEdit.text() != '' and
+            len(self.phone_lineEdit.text()) == 17):
+            requests.add_library(self.library_name_lineEdit.text(),
+                                 self.adress_lineEdit.text(),
+                                 self.open_timeEdit.time().toString("HH:mm"),
+                                 self.close_timeEdit.time().toString("HH:mm"),
+                                 self.phone_lineEdit.text().replace("-", "").replace("(", "").replace(")", ""),
+                                 self.computer_class_checkBox.isChecked(),
+                                 self.reading_room_checkBox.isChecked())
+        else:
+            self.error_label.setText("Заполните все поля!")
+
+class ChangeStaffInfoWindow(QDialog):
+    def __init__(self):
+        super(ChangeStaffInfoWindow, self).__init__()
+        uic.loadUi('Ui\\change_staff_info_window.ui', self)
+        self.pushButton.clicked.connect(self.change_data)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
+        readers = requests.parse_notes("Personal")
+        row = 0
+        for employee in readers:
+            self.empoyeeTable.insertRow(self.empoyeeTable.rowCount())
+            for column, value in enumerate(employee):
+                item = QTableWidgetItem(str(value).rstrip())
+                if column == 0 or column == 2 or column == 1:
+                    item.setFlags(Qt.ItemFlag.ItemIsEditable)
+                self.empoyeeTable.setItem(row, column, item)
+            row +=1
+
+    def change_data(self):
+        all_rows = []
+        num_rows = self.empoyeeTable.rowCount()
+        num_columns = self.empoyeeTable.columnCount()
+        for row in range(num_rows):
+            current_row = []
+            for column in range(num_columns):
+                item = self.empoyeeTable.item(row, column)
+                if item is not None:
+                    current_row.append(item.text())
+                else:
+                    current_row.append("")  # Если ячейка пуста, добавляем пустую строку
+            all_rows.append(current_row)
+        requests.update_employee_info(all_rows)
 
 class UserInfoWindow(QDialog):
     def __init__(self):
         super(UserInfoWindow, self).__init__()
         uic.loadUi('Ui\\user_info_window.ui', self)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
         if info[0] == "Personal":
             self.name_value_label.setText(info[1][3])
             self.surname_value_label.setText(info[1][4])
             self.patronymic_value_label.setText(info[1][5])
             self.library_email_value_label.setText(info[1][2])
-            self.ticket_contract_number_value_label.setText(info[1][1])
+            self.ticket_contract_number_value_label.setText(str(info[1][1]))
+            self.ticket_contract_number_label.setText("Номер договора:")
             self.pasport_phone_value_label.setText(info[1][6])
             self.SNILS_birthdate_value_label.setText(info[1][7])
             self.INN_value_label.setText(info[1][8])
@@ -209,9 +474,13 @@ class BookAddWindow(QDialog):
         self.Library_comboBox.addItems(requests.select_libraries())
         self.Add_pushButton.clicked.connect(self.add)
         self.Image_pushButton.clicked.connect(self.browsefiles)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
+
     def browsefiles(self):
         fname = QFileDialog.getOpenFileName(self, "Выбор картинки", "C:\\Users\\khmel\\Desktop\\6 семестр\\СУБД\\LibraryProject\\images", "Image Files (*.png *.jpg *.jpeg)")
         self.Image_lineEdit.setText(fname[0])
+
     def add(self):
         if (self.Library_comboBox.currentIndex() != 0 and self.Title_lineEdit.text() != "" and
                 self.Author_lineEdit.text() != "" and self.Language_lineEdit.text() != "" and
@@ -239,6 +508,8 @@ class BookRemoveWindow(QDialog):
         self.Library_comboBox.addItems(requests.select_libraries())
         self.Library_comboBox.currentTextChanged.connect(self.change_books_list)
         self.error_label.hide()
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
 
     def change_books_list(self):
         self.book_comboBox.clear()
@@ -271,6 +542,8 @@ class ChangeNumberOfBooksWindow(QDialog):
     def __init__(self):
         super(ChangeNumberOfBooksWindow, self).__init__()
         uic.loadUi('Ui\\number_of_books_window.ui', self)
+        icon = QIcon(":/images/images/icon.ico")
+        self.setWindowIcon(icon)
         self.quantity_spinBox.clear()
         self.Library_comboBox.addItems(requests.select_libraries())
         self.Library_comboBox.currentTextChanged.connect(self.change_books_list)
--- requests.py
+++ requests.py
@@ -1,6 +1,74 @@
+from datetime import datetime, timedelta
 import pyodbc
 
 con = pyodbc.connect(Trusted_Connection='yes', driver='{SQL Server}', server='ETNA\\SQLEXPRESS', database='Library')
+
+def del_library(lib_name):
+    cursor = con.cursor()
+    cursor.execute("DELETE FROM Libraries WHERE Library_Name = '{}'".format(lib_name))
+    con.commit()
+
+def add_library(name, adress, open, close, phone, pc, room):
+    cursor = con.cursor()
+    cursor.execute(
+        "INSERT INTO Libraries (Library_Name, Address, Opening_Hours, Closing_Hours,"
+        " Contact_Number, Computer_Class, Reading_Room) VALUES (?,?,?,?,?,?,?)",
+        name, adress, open, close, phone, pc, room)
+    con.commit()
+
+def update_info(table, column, key_column, key, new_value):
+    cursor = con.cursor()
+    cursor.execute("UPDATE {} SET {} = '{}' WHERE {} = '{}'".format(
+        table, column, new_value, key_column, key))
+    con.commit()
+
+def del_expired_books():
+    cursor = con.cursor()
+    cursor.execute("UPDATE Books SET Quantity = Quantity + 1 WHERE EXISTS(SELECT 1 FROM Rented_Books WHERE CAST(Return_Date AS DATETIME) < GETDATE());")
+    cursor.execute("DELETE FROM Rented_Books WHERE CAST(Return_Date AS DATETIME) < GETDATE()")
+    con.commit()
+
+def rent_book(book_info, login):
+    cursor = con.cursor()
+    current_date = datetime.now()
+    one_month_later = (current_date + timedelta(days=30))
+    cursor.execute("INSERT INTO Rented_Books (Book_Id, Return_Date, Reader_Login) VALUES (?,?,?)",
+        int(book_info[7]), one_month_later, login)
+    cursor.execute("UPDATE Books SET Quantity = Quantity - 1 WHERE Book_Id = '{}'".format(book_info[7]))
+    con.commit()
+
+def search_book(id):
+    cursor = con.cursor()
+    cursor.execute("SELECT Library, Author, Title, Release_Date, Language, Description, Quantity, Book_Id "
+                   "FROM Library.dbo.Books WHERE Book_Id='{}'".format(id))
+    return cursor.fetchone()
+
+def update_users_info(new_table):
+    cursor = con.cursor()
+    cursor.execute("DELETE FROM Readers")
+    for row in new_table:
+        cursor.execute("INSERT INTO Readers (Login, Name, Surname, Patronymic, Birth_Date, Contact_Number, Email) "
+                       "VALUES (?,?,?,?,?,?,?)",
+                       row[0], row[2], row[3], row[4], row[5], row[6], row[7])
+    con.commit()
+
+def update_library_info(lib, adr, op_hr, cls_hr, p_num, pc, rm):
+    cursor = con.cursor()
+    print("UPDATE Libraries SET Address = {}, Opening_Hours = {}, Closing_Hours = {}, "
+                   "Contact_Number = {}, Computer_Class = {}, Reading_Room = {} WHERE Library_Name = {}".format(
+        adr, op_hr, cls_hr, p_num, pc, rm, lib))
+    cursor.execute("UPDATE Libraries SET Address = '{}', Opening_Hours = '{}', Closing_Hours = '{}', Contact_Number = '{}', Computer_Class = '{}', Reading_Room = '{}' WHERE Library_Name = '{}'".format(
+        adr, op_hr, cls_hr, p_num, pc, rm, lib))
+    con.commit()
+
+def update_employee_info(new_table):
+    cursor = con.cursor()
+    cursor.execute("DELETE FROM Personal")
+    for row in new_table:
+        cursor.execute("INSERT INTO Personal (Login, Library, Name, Surname, Patronymic, Passport_Number, SNILS_Number, INN_Number) "
+                       "VALUES (?,?,?,?,?,?,?,?)",
+                       row[0], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
+    con.commit()
 
 def is_row_exist(table, column, value):
     cursor = con.cursor()
@@ -78,6 +146,11 @@ def notes_count(table):
     cursor.execute("SELECT COUNT(*) FROM {}".format(table))
     for string in cursor:
         return string[0]
+
+def parse_row(table, column_key, key):
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM {} WHERE {} = '{}'".format(table, column_key, key))
+    return cursor.fetchone()
 
 def parse_notes(table):
     cursor = con.cursor()
